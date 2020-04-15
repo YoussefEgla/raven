3b20ea0200e7521ac6ccf02aa914b099ea818a77
Youssef AbouEgla <youssefegla@gmail.com>
2020-04-15 17:35:34 +0200

    user model & db connection

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const User = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profileImageUrl: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", User);
