dd81d9fc0f8c41741a85c6f828631288a1ecbe3b
Youssef AbouEgla <youssefegla@gmail.com>
2020-04-15 20:54:21 +0200

    authenticate user

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+// make sure the user is logged in - authentication
+exports.loginRequired = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, function (err, payload) {
+      if (payload) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: "Please login first",
+        });
+      }
+    });
+  } catch (err) {
+    return next({
+      status: 401,
+      message: "Please login first",
+    });
+  }
+};
+// make sure we get the correct user - authorization
+exports.ensureCorrectUser = function (req, res, next) {};
