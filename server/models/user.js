9c3a409381621cb6588a55b9e194701cc8db2aac
Youssef AbouEgla <youssefegla@gmail.com>
2020-04-15 17:44:04 +0200

    password hash and compare

--- server/models/user.js
+++ server/models/user.js
@@ -21,4 +21,29 @@ const User = new mongoose.Schema({
   },
 });
 
+User.pre("save", async (next) => {
+  try {
+    // @ts-ignore
+    if (!this.isModified("password")) {
+      return next();
+    }
+    // @ts-ignore
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// @ts-ignore
+User.method.comparePassword = async (candidatePassword, next) => {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
+
 module.exports = mongoose.model("User", User);
