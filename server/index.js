345dfda7925bd274e6d2c59496344c4f7be82add
Youssef AbouEgla <youssefegla@gmail.com>
2020-04-15 21:51:17 +0200

    bug fix

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(errorHandler);
 /**
  * Start Server
  */
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Servr started at port ${PORT}`);
 });
